418b4a0de7dcb84a57b8951c83057f81a2a0e227
Ratumillenia12 <61896787+Ratumillenia12@users.noreply.github.com>
2020-05-19 20:51:53 +0800

    Add files via upload

---
+++ Swing.java
@@ -0,0 +1,14 @@
+package no2;
+
+import javax.swing.*;
+class Swing extends Jframe{
+	Swing(){
+		super("INI SWING LHO...");
+		setSize(300,300);
+		show();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+	public static void main(String[] args) {
+		Swing sw=new Swing();
+	} 
+}
